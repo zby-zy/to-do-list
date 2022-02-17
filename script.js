14825a8a8915a291f965b2e8b884cb1fbbe8c113
Zeyneb Bayrak <85934122+zby-zy@users.noreply.github.com>
2022-02-17 18:25:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-document.querySelector('#push').onclick = function() {
-    if (document.querySelector('#newtask input').value.length == 0) {
+document.querySelector('#push').onclick = function(){
+    if (document.querySelector('#newtask input').value.length == 0){
         alert("Please Enter a Task")
     } else {
         document.querySelector('#tasks').innerHTML += `
@@ -14,14 +14,14 @@ document.querySelector('#push').onclick = function() {
         `;
 
         var current_tasks = document.querySelectorAll(".delete");
-        for (var i = 0; i < current_tasks.length; i++) {
-            current_tasks[i].onclick = function() {
+        for(var i = 0; i < current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
                 this.parentNode.remove();
             }
         }
 
         var tasks = document.querySelectorAll(".task");
-        for (var i = 0; i < tasks.length; i++) {
+        for(var i = 0; i < tasks.length; i++) {
             tasks[i].onclick = function() {
                 this.classList.toggle('completed');
             }
